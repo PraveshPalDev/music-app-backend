855d21343ae3b7853b4ec336dff45ec8d191cfdd
zenesys0081 <pravesh.kumar2@zenesys.com>
2025-04-10 12:59:16 +0530

    fixed the success response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,15 @@ import {ApiError} from '../utils/apiErrorHandler.js';
 
 export const getAllUsers = async (req, res, next) => {
   try {
-    const users = [];
+    const users = [
+      {
+        name: 'Adeel Solangi',
+        language: 'Sindhi',
+        id: 'V59OF92YF627HFY0',
+        bio: 'Donec lobortis eleifend condimentum. Cras dictum dolor lacinia lectus vehicula rutrum. Maecenas quis nisi nunc. Nam tristique feugiat est vitae mollis. Maecenas quis nisi nunc.',
+        version: 6.1,
+      },
+    ];
 
     if (!users || users.length === 0) {
       throw new ApiError(404, 'No users found');
